a84407048fab2f38175a2c93f426dbf22dfee53a
Shreehanna <shreehannamathew24@gmail.com>
2025-07-23 23:00:28 +0530

    Initial commit – added basic C programs

---
+++ logic_gates.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+int main(){
+    int a;
+    scanf("%d",&a);
+    if (a>0){
+        printf("positive\n");}
+    else if (a<0){
+        printf("negative\n");}
+    else{
+        printf("zero");}
+    return 0;
+}
